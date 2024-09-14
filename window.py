3a8aa97df930b729fcb5119116f1ca68aff3aff4
John Phillips <johnphillips114@gmail.com>
2024-09-14 08:53:21 -0600

    Created window model

---
+++ window.py
@@ -0,0 +1,32 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title = ""
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root)
+        self.__canvas.pack()
+        self.is_running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        print("Redrawing")
+        while self.is_running == True:
+            self.redraw()
+        print("Stopped redrawing")
+    
+    def close(self):
+        self.is_running = False
+
+    def debug_print(self):
+        print(f"Window width: {self.width}")
+        print(f"Window height: {self.height}")
+        print(f"Window is running: {self.is_running}")
